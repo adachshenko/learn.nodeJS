3810efbfaa3b2fe5f496de1819ad302b66e92fb1
Darya_Adachshenko <Darya_Adachshenko@epam.com>
2017-10-27 20:21:05 +0600

    Task 3.2

--- utils/streams.js
+++ utils/streams.js
@@ -1,9 +1,28 @@
-process.argv.forEach((val, index) => {
+//import * as parseArgs from 'minimist';
+/*process.argv.forEach((val, index) => {
     console.log(`${index}: ${val}`);
+});*/
+
+let argsArr = process.argv;
+
+let args = require('minimist')(argsArr.slice(2), {
+    alias: {
+        'help': 'h',
+        'action': 'a'
+    },
+    unknown: (arg) => {
+        if (arg !== 'help' && arg !== 'h' && arg !== 'action' && arg !== 'a') {
+            console.error('Unknown option: ', arg);
+            return false;
+        }
+    }
 });
+console.dir(args);
 
-let args = process.argv;
-
-if (args[2] === '--help') {
+if (args.help) {
     console.log('superman is here!');
+}
+if (argsArr.length <= 2){
+    console.log('Incorrect input. Use --action="nameAction"/-a "nameAction" for executing an action. ' +
+        '\nOr --help/-h for getting more information about using of this program');
 }

769dd98d2134535e55923771a7df3a8d1531f1c9
Darya_Adachshenko <Darya_Adachshenko@epam.com>
2017-10-30 19:22:11 +0600

    Task 3.3

--- utils/streams.js
+++ utils/streams.js
@@ -1,48 +1,70 @@
-//import * as parseArgs from 'minimist';
-/*process.argv.forEach((val, index) => {
-    console.log(`${index}: ${val}`);
-});*/
+var fs = require('fs');
 
 let argsArr = process.argv;
 
 let args = require('minimist')(argsArr.slice(2), {
     alias: {
         'help': 'h',
-        'action': 'a'
+        'action': 'a',
+        'file': 'f'
     },
-    /*unknown: (arg) => {
+    unknown: (arg) => {
         if (arg !== 'file' && arg !== 'f' && arg !== 'help' && arg !== 'h' && arg !== 'action' && arg !== 'a') {
             console.error('Unknown option: ', arg);
             return false;
         }
-    }*/
+    }
 });
 console.dir(args);
 
 if (args.help) {
     printHelpMessage();
 }
-if (argsArr.length <= 2){
+if (args.action === 'read' && args.file !== undefined) {
+    readFile(args.file);
+}
+if (args.action === 'upperCase') {
+    convertToUpperCase();
+}
+if (argsArr.length <= 2) {
     console.log('Incorrect input. Use --action="nameAction"/-a "nameAction" for executing an action. ' +
         '\nOr --help/-h for getting more information about using of this program');
 }
 
-function inputOutput(filePath){
-    process.stdin.pipe(process.stdout);
+function readFile(filePath) {
+    let readStream = fs.createReadStream(filePath);
+    readStream.on('open', function () {
+        readStream.pipe(process.stdout);
+    });
+
+    readStream.on('error', function (err) {
+        process.stdout.write(`Error during reading file ${err}`);
+    });
+}
+
+function convertToUpperCase() {
+    process.stdin.setEncoding('utf8');
+    process.stdin.on('readable', () => {
+        const chunk = process.stdin.read();
+        if (chunk !== null) {
+            process.stdout.write(`You have entered : ${chunk.toUpperCase()}`);
+        }
+    });
+
+    process.stdin.on('end', () => {
+        process.stdout.write('end');
+    });
 
 }
-function transformFile(filePath) {
+
+function convertCsvToJson() {
 
 }
-function transform() {
+
+function cssBundler() {
 
 }
-function httpClient() {
 
-}
-function httpServer() {
-
-}
 function printHelpMessage() {
-    console.log('superman is here!ffffffff');
+    console.log('superman is here!');
 }

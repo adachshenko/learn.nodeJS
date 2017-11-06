721090b5f14d842b064fe87e4bb5774dc0c0b344
Darya_Adachshenko <Darya_Adachshenko@epam.com>
2017-11-06 18:52:18 +0600

    Task 4: Added plain-text-server.js

---
+++ http_server/plain-text-server.js
@@ -0,0 +1,8 @@
+//import * as http from 'http';  ?!
+var http = require('http');
+
+http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World');
+    res.end();
+}).listen(8000);
